5f05bc61d0fa529daf70096f40ed631a0f0a21cf
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-05 20:10:17 -0300

    Injeção de Dependências

--- libpythonpro_mlpdf/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_mlpdf/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -20,7 +20,7 @@ from libpythonpro_mlpdf.spam.modelos import Usuario
 def test_qte_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'matheuslopes.pdf@gmail.com',
@@ -28,3 +28,32 @@ def test_qte_de_spam(sessao, usuarios):
         'Corpo'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Matheus', email='matheuslopes.pdf@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'teste@gmail.com',
+        'Curso Python Pro',
+        'Corpo'
+    )
+    assert enviador.parametros_de_envio == (
+        'teste@gmail.com',
+        'matheuslopes.pdf@gmail.com',
+        'Curso Python Pro',
+        'Corpo'
+    )
