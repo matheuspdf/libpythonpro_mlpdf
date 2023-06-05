758fa8b145f17d9abe3534453530405c3c61d5bd
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-05 19:46:40 -0300

    Produção de Código Testável

--- libpythonpro_mlpdf/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_mlpdf/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro_mlpdf.spam.enviador_de_email import Enviador
 from libpythonpro_mlpdf.spam.main import EnviadorDeSpam
+from libpythonpro_mlpdf.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao=None):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Matheus', email='matheuslopes.pdf@gmail.com'),
+            Usuario(nome='Gabriel', email='matheuslopes.pdf@gmail.com')
+        ],
+        [
+            Usuario(nome='Gabriel', email='matheuslopes.pdf@gmail.com')
+        ]
+    ]
+)
+def test_qte_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'matheuslopes.pdf@gmail.com',
         'Curso Python Pro',
         'Corpo'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
--- libpythonpro_mlpdf/tests/test_spam/test_usuarios.py
+++ libpythonpro_mlpdf/tests/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from libpythonpro_mlpdf.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Matheus')
+    usuario = Usuario(nome='Matheus', email='matheuslopes.pdf@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Matheus'), Usuario(nome='Gabriel')]
+    usuarios = [Usuario(nome='Matheus', email='matheuslopes.pdf@gmail.com'),
+                Usuario(nome='Gabriel', email='matheuslopes.pdf@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
