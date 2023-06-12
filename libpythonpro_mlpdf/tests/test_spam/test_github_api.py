a897c46bf64fb38e42d4c925a346b4e00116bf6c
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-12 20:29:42 -0300

    Biblioteca Pytest-Mock

--- libpythonpro_mlpdf/tests/test_spam/test_github_api.py
+++ libpythonpro_mlpdf/tests/test_spam/test_github_api.py
@@ -6,21 +6,20 @@ from libpythonpro_mlpdf import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/96511087?v=4'
     resp_mock.json.return_value = {
         "login": "matheuspdf", "id": 96511087,
         "avatar_url": url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro_mlpdf.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
-    url = github_api.buscar_avatar('matheuspdff')
+    url = github_api.buscar_avatar('matheus')
     assert avatar_url == url
 
 
